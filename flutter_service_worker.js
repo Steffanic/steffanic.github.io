14ad0ad20102a438e72368a4718556af08faaf9f
Steffanic <collidingpaticles@gmail.com>
2022-05-05 18:49:20 -0400

    Pushing v0.95

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -137,10 +137,10 @@ const RESOURCES = {
 "icons/number-7.png": "5c76f6c57e326ab320f2394440f90487",
 "icons/number-8.png": "5a315871a127ceee481c09471875e810",
 "icons/number-9.png": "32677bfeb2626644c761164c6384a2f0",
-"index.html": "6e04c8c25d30dd0370e527d04927aa3a",
-"/": "6e04c8c25d30dd0370e527d04927aa3a",
+"index.html": "761290e4a57a84ae72aab9504302a864",
+"/": "761290e4a57a84ae72aab9504302a864",
 "leaflet.timedimension.control.min.css": "cb8040b8f2c12d5820314ebaeb107066",
-"main.dart.js": "065e20596eb118d1c51bd00f7aafc96e",
+"main.dart.js": "7e1267ee722101d427987ce404ec8b27",
 "manifest.json": "1c1b35bea54569d3411e68a6510fb942",
 "version.json": "75a41e110323d5fd688ed9dbf45133e3"
 };
